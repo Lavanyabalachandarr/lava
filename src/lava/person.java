b8a994884dd75df1f39c884bccefc88597b02694
S.P.Bala <S.P.Bala@LAPTOP-SED5QB0S>
2019-02-22 10:45:51 +0530

    testing

---
+++ src/lava/person.java
@@ -0,0 +1,26 @@
+package lava;
+
+public class person {
+	 
+	 private int age;
+	 private String name;
+	
+	public person (int age, String name) {
+		this.age=age;
+		this.name=name;
+	}
+
+	
+	public void display()
+	{
+		System.out.println("the person age is:"+age);
+	System.out.println("the person name is:"+name);
+		
+	}
+}
+	
+
+		
+
+
+
